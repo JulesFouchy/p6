1421d351c4dc08b3bc5077ae8d189d386af84f00
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-10 13:02:54 +0100

    Start refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -307,67 +307,84 @@ impl State {
         self.uniforms.update_model_matrix(x, y, w, h, rotation);
         self.rect_with_shader(&self.ellipse_render_pipeline);
     }
+}
 
-    pub fn run() {
+pub trait AppP2D {
 
-        env_logger::init();
-        let event_loop = EventLoop::new();
-        let window = WindowBuilder::new().build(&event_loop).unwrap();
-    
-        use futures::executor::block_on;
-    
-        // Since main can't be async, we're going to need to block
-        let mut state = block_on(State::new(&window));
-    
-        event_loop.run(move |event, _, control_flow| {
-            match event {
-                Event::WindowEvent {
-                    ref event,
-                    window_id,
-                } if window_id == window.id() => {
-                    if !state.input(event) {
-                        match event {
-                            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                            WindowEvent::KeyboardInput { input, .. } => match input {
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                } => *control_flow = ControlFlow::Exit,
-                                _ => {}
-                            },
-                            WindowEvent::Resized(physical_size) => {
-                                state.resize(*physical_size);
-                            }
-                            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                                // new_inner_size is &mut so w have to dereference it twice
-                                state.resize(**new_inner_size);
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-                Event::RedrawRequested(_) => {
-                    state.update();
-                    match state.begin_rendering() {
-                        Ok(_) => {}
-                        // Recreate the swap_chain if lost
-                        Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
-                        // The system is out of memory, we should probably quit
-                        Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                        // All other errors (Outdated, Timeout) should be resolved by the next frame
-                        Err(e) => eprintln!("{:?}", e),
-                    }
-                    state.render();
-                    state.end_rendering();
-                }
-                Event::MainEventsCleared => {
-                    // RedrawRequested will only trigger once, unless we manually
-                    // request it.
-                    window.request_redraw();
-                }
-                _ => {}
+}
+
+pub struct p6 {
+      state: State,
+      event_loop: winit::event_loop::EventLoop<()>,
+      window: winit::window::Window,
+}
+
+impl p6 {
+      pub fn new() -> Self {        
+            env_logger::init();
+            let event_loop = EventLoop::new();
+            let window = WindowBuilder::new().build(&event_loop).unwrap();
+        
+            use futures::executor::block_on;
+            // Since main can't be async, we're going to need to block
+            let state = block_on(State::new(&window));
+            Self {
+                  state,
+                  event_loop,
+                  window,
             }
-        });
-    }
+      }
+
+      pub fn run(&'static self) {
+            self.event_loop.run(move |event, _, control_flow| {
+                  match event {
+                      Event::WindowEvent {
+                          ref event,
+                          window_id,
+                      } if window_id == self.window.id() => {
+                          if !self.state.input(event) {
+                              match event {
+                                  WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                                  WindowEvent::KeyboardInput { input, .. } => match input {
+                                      KeyboardInput {
+                                          state: ElementState::Pressed,
+                                          virtual_keycode: Some(VirtualKeyCode::Escape),
+                                          ..
+                                      } => *control_flow = ControlFlow::Exit,
+                                      _ => {}
+                                  },
+                                  WindowEvent::Resized(physical_size) => {
+                                    self.state.resize(*physical_size);
+                                  }
+                                  WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                                      // new_inner_size is &mut so w have to dereference it twice
+                                      self.state.resize(**new_inner_size);
+                                  }
+                                  _ => {}
+                              }
+                          }
+                      }
+                      Event::RedrawRequested(_) => {
+                        self.state.update();
+                          match self.state.begin_rendering() {
+                              Ok(_) => {}
+                              // Recreate the swap_chain if lost
+                              Err(wgpu::SwapChainError::Lost) => self.state.resize(self.state.size),
+                              // The system is out of memory, we should probably quit
+                              Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                              // All other errors (Outdated, Timeout) should be resolved by the next frame
+                              Err(e) => eprintln!("{:?}", e),
+                          }
+                          self.state.render();
+                          self.state.end_rendering();
+                      }
+                      Event::MainEventsCleared => {
+                          // RedrawRequested will only trigger once, unless we manually
+                          // request it.
+                          self.window.request_redraw();
+                      }
+                      _ => {}
+                  }
+              });
+      }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use p6::*;
 
+struct MyApp {
+    state: State,
+}
+
+impl MyApp {
+    fn new() -> Self {
+        Self(State::new(window: &Window))
+    }
+}
+
+impl AppP2D for MyApp {
+
+}
+
 fn main() {
     State::run();
 }
