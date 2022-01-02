da59b56172b94ea87b0ab7f9b3afa7879f5c8528
Jules Fouchy <jules.fouchy@ntymail.com>
2022-01-02 01:25:39 +0100

    [math] Add map

--- include/p6/p6.h
+++ include/p6/p6.h
@@ -3,3 +3,4 @@
 #include "../../src/Context.h"
 #include "../../src/Image.h"
 #include "../../src/Shader.h"
+#include "../../src/math.h"
---
+++ src/math.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace p6 {
+
+/// Maps x, which is in the range [from_mix, from_max], to the range [to_min, to_max]
+/// For example if x == from_min, then the function returns to_min
+/// if x == (from_min + from_max) / 2 then the function returns (to_min + to_max) / 2
+/// if x is outside of the range [from_min, from_max] the result will be outside of [to_min, to_max], the same linear behaviour still applies
+template<typename T>
+T map(T x, T from_min, T from_max, T to_min, T to_max)
+{
+    return (x - from_min) * (to_max - to_min) / (from_max - from_min) + to_min;
+}
+
+} // namespace p6
