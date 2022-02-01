6856fd53ea6d220b6ef12a33a6e8c806492cbb98
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-01 21:37:18 +0100

    [Image] Cleanup documentation

--- src/Image.h
+++ src/Image.h
@@ -15,22 +15,22 @@ using Texture   = glpp::Texture;
 
 /// An image can be drawn onto, and can be displayed.
 /// If you plan on drawing on the image, you can create it without data (it will create a black and transparent image).
-/// If you want to load an image, you can use load_image() instead of the constructor
+/// If you want to load an image, you can use load_image() instead of the constructor.
 class Image {
 public:
-    /// data must be nullptr, or an array of size size.width() * size.height() * 4, with R, G, B and A channels, starting with the bottom left pixel, and going row by row
+    /// data must be nullptr, or an array of size `size.width() * size.height() * 4`, with R, G, B and A channels, starting with the bottom left pixel, and going row by row.
     explicit Image(ImageSize size, const uint8_t* data = nullptr);
 
-    /// Returns the size in pixels of the image
+    /// Returns the size in pixels of the image.
     ImageSize size() const { return _render_target.size(); }
 
-    /// Returns the internal texture, that you can use to do advanced stuff with custom shaders
+    /// Returns the internal texture, that you can use to do advanced stuff with custom shaders.
     const Texture& texture() const { return _render_target.texture(); }
 
-    /// Resizes the image
-    /// /!\ All of its content is lost. If you don't want that you can use resize() instead
+    /// Resizes the image.
+    /// /!\ All of its content is lost. If you don't want that you can use resize() instead.
     void destructive_resize(ImageSize size) { _render_target.resize(size); }
-    /// Resizes the image
+    /// Resizes the image.
     void resize(ImageSize size) { _render_target.conservative_resize(size); }
 
 private:
