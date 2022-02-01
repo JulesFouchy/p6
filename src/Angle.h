b68cd7538418c8e87c8b849ad22b1fdcfb0ffd69
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-01 13:28:44 +0100

    [Angle] Add

---
+++ src/Angle.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <op/op.hpp>
+#include "math.h"
+
+namespace p6 {
+
+struct Radians
+    : public op::Addable<Radians>
+    , public op::Subtractable<Radians>
+    , public op::Negatable<Radians>
+    , public op::Scalable<Radians> {
+    float value;
+
+    explicit Radians(float value)
+        : value{value} {}
+};
+
+inline Radians turns_to_radians(float turns)
+{
+    return Radians{turns * TAU};
+}
+
+inline Radians degrees_to_radians(float degrees)
+{
+    return Radians{degrees / 360.f * TAU};
+}
+
+inline float radians_to_degrees(Radians radians)
+{
+    return radians.value / TAU * 360.f;
+}
+
+inline float radians_to_turns(Radians radians)
+{
+    return radians.value / TAU;
+}
+
+class Angle
+    : public op::Addable<Angle>
+    , public op::Subtractable<Angle>
+    , public op::Negatable<Angle>
+    , public op::Scalable<Angle> {
+public:
+    explicit Angle(Radians value)
+        : value{value} {}
+
+    float as_turns() const { return radians_to_turns(value); }
+    float as_radians() const { return value.value; }
+    float as_degrees() const { return radians_to_degrees(value); }
+
+    Radians value;
+
+private:
+    // Radians value;
+};
+
+} // namespace p6
+
+inline p6::Angle operator"" _turns(long double turns)
+{
+    return p6::Angle{p6::turns_to_radians(static_cast<float>(turns))};
+}
+
+inline p6::Angle operator"" _degrees(long double degrees)
+{
+    return p6::Angle{p6::degrees_to_radians(static_cast<float>(degrees))};
+}
+
+inline p6::Angle operator"" _radians(long double radians)
+{
+    return p6::Angle{p6::Radians{static_cast<float>(radians)}};
+}
