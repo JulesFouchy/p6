6a1328a1b48ca51a8da13bba64807927b7212f27
dsmtE <enguerrand.desmet@gmail.com>
2022-02-27 12:46:17 +0100

    [Transform2D] move definition from context to Transfrom2D

--- src/Context.h
+++ src/Context.h
@@ -22,102 +22,6 @@
 
 namespace p6 {
 
-struct Center {
-    glm::vec2 value{0.f};
-
-    Center() = default;
-
-    Center(float x, float y)
-        : value{x, y} {}
-
-    Center(glm::vec2 value)
-        : value{value} {}
-};
-
-struct TopLeftCorner {
-    glm::vec2 value{0.f};
-
-    TopLeftCorner() = default;
-
-    TopLeftCorner(float x, float y)
-        : value{x, y} {}
-
-    TopLeftCorner(glm::vec2 value)
-        : value{value} {}
-};
-
-struct TopRightCorner {
-    glm::vec2 value{0.f};
-
-    TopRightCorner() = default;
-
-    TopRightCorner(float x, float y)
-        : value{x, y} {}
-
-    TopRightCorner(glm::vec2 value)
-        : value{value} {}
-};
-
-struct BottomLeftCorner {
-    glm::vec2 value{0.f};
-
-    BottomLeftCorner() = default;
-
-    BottomLeftCorner(float x, float y)
-        : value{x, y} {}
-
-    BottomLeftCorner(glm::vec2 value)
-        : value{value} {}
-};
-
-struct BottomRightCorner {
-    glm::vec2 value{0.f};
-
-    BottomRightCorner() = default;
-
-    BottomRightCorner(float x, float y)
-        : value{x, y} {}
-
-    BottomRightCorner(glm::vec2 value)
-        : value{value} {}
-};
-
-struct Radii {
-    glm::vec2 value{1.f};
-
-    Radii() = default;
-
-    Radii(float x, float y)
-        : value{x, y} {}
-
-    Radii(glm::vec2 v)
-        : value{v} {}
-};
-
-struct Radius {
-    float value{1.f};
-
-    Radius() = default;
-    Radius(float value)
-        : value{value} {};
-};
-
-struct RadiusX {
-    float value{1.f};
-
-    RadiusX() = default;
-    RadiusX(float value)
-        : value{value} {};
-};
-
-struct RadiusY {
-    float value{1.f};
-
-    RadiusY() = default;
-    RadiusY(float value)
-        : value{value} {};
-};
-
 struct FullScreen {
 };
 
@@ -415,16 +319,7 @@ private:
 
     Transform2D make_transform_2D_impl(glm::vec2 offset_to_center, glm::vec2 corner_position, Radii radii, Rotation rotation) const;
     Transform2D make_transform_2D(FullScreen) const;
-    Transform2D make_transform_2D(Center, Radius, Rotation) const;
-    Transform2D make_transform_2D(Center, Radii, Rotation) const;
-    Transform2D make_transform_2D(TopLeftCorner, Radius, Rotation) const;
-    Transform2D make_transform_2D(TopLeftCorner, Radii, Rotation) const;
-    Transform2D make_transform_2D(TopRightCorner, Radius, Rotation) const;
-    Transform2D make_transform_2D(TopRightCorner, Radii, Rotation) const;
-    Transform2D make_transform_2D(BottomLeftCorner, Radius, Rotation) const;
-    Transform2D make_transform_2D(BottomLeftCorner, Radii, Rotation) const;
-    Transform2D make_transform_2D(BottomRightCorner, Radius, Rotation) const;
-    Transform2D make_transform_2D(BottomRightCorner, Radii, Rotation) const;
+
 
 private:
     mutable details::UniqueGlfwWindow _window;
