b3b45f314372f42803eea3e60aea7fe64fba9f42
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-08 22:46:11 +0200

    [Clock_Realtime] Miscellaneous improvements

--- src/details/Time/Clock_Realtime.cpp
+++ src/details/Time/Clock_Realtime.cpp
@@ -4,14 +4,16 @@ namespace p6 {
 namespace details {
 
 Clock_Realtime::Clock_Realtime()
-    : _initial_time(std_time()), _last_time(std_time()), _current_time(std_time())
+    : _initial_time(std_time())
+    , _last_time(std_time())
+    , _current_time(std_time())
 {
 }
 
 float Clock_Realtime::delta_time() const
 {
     if (is_playing()) {
-        std::chrono::duration<float> dt = _current_time - _last_time;
+        const std::chrono::duration<float> dt = _current_time - _last_time;
         return dt.count();
     }
     else {
@@ -21,13 +23,14 @@ float Clock_Realtime::delta_time() const
 
 float Clock_Realtime::time() const
 {
-    std::chrono::duration<float> delta = _current_time - _initial_time;
-    return delta.count() - _offset_with_std_time;
+    const std::chrono::duration<float> t = _current_time - _initial_time - _offset_with_std_time;
+    return t.count();
 }
 
 void Clock_Realtime::set_time(float new_time)
 {
-    _offset_with_std_time += time() - new_time;
+    const std::chrono::nanoseconds new_chrono_time{static_cast<std::chrono::nanoseconds::rep>(1000000000.f * new_time)};
+    _offset_with_std_time = _current_time - _initial_time - new_chrono_time;
 }
 
 void Clock_Realtime::update()
@@ -41,9 +44,10 @@ void Clock_Realtime::update()
 void Clock_Realtime::play()
 {
     Clock::play();
-    std::chrono::duration<float> delta = std_time() - _time_when_paused;
-    _offset_with_std_time += delta.count();
-    update();
+    const auto delta = std_time() - _time_when_paused;
+    _offset_with_std_time += delta;
+    _last_time += delta;
+    _current_time += delta;
 }
 
 void Clock_Realtime::pause()
--- src/details/Time/Clock_Realtime.h
+++ src/details/Time/Clock_Realtime.h
@@ -9,7 +9,6 @@ namespace details {
 class Clock_Realtime : public Clock {
 public:
     Clock_Realtime();
-    ~Clock_Realtime() = default;
 
     float delta_time() const override;
     float time() const override;
@@ -21,14 +20,14 @@ public:
     void pause() override;
 
 private:
-    inline std::chrono::steady_clock::time_point std_time() { return std::chrono::steady_clock::now(); }
+    std::chrono::steady_clock::time_point std_time() { return std::chrono::steady_clock::now(); }
 
 private:
     std::chrono::steady_clock::time_point _initial_time;
     std::chrono::steady_clock::time_point _last_time;
     std::chrono::steady_clock::time_point _current_time;
     std::chrono::steady_clock::time_point _time_when_paused;
-    float                                 _offset_with_std_time = 0.f; // Allows us to set_time as we wish
+    std::chrono::nanoseconds              _offset_with_std_time{0}; // Allows us to set_time as we wish
 };
 
 } // namespace details
