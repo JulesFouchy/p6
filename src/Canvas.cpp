38a96f12d735e28bf2156083b7e67263b8b7da22
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-05 10:38:07 +0200

    ✨ [save_canvas] Make path relative to exe and create intermediate folders if necessary

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -1,5 +1,7 @@
 #include "Canvas.h"
 #include <img/img.hpp>
+#include "internal/make_absolute_path.h"
+#include "internal/make_directories_if_necessary.h"
 
 namespace p6 {
 
@@ -14,7 +16,9 @@ void save_canvas(const Canvas& canvas, std::filesystem::path path)
     const auto                 height = canvas.size().height();
     std::unique_ptr<uint8_t[]> data{new uint8_t[4 * width * height]};
     glReadPixels(0, 0, static_cast<GLsizei>(width), static_cast<GLsizei>(height), GL_RGBA, GL_UNSIGNED_BYTE, data.get());
-    img::save_png(path.string().c_str(), width, height, data.get(), 4);
+    const auto absolute_path = internal::make_absolute_path(path);
+    internal::make_directories_if_necessary(absolute_path);
+    img::save_png(absolute_path.string().c_str(), width, height, data.get(), 4);
 }
 
 } // namespace p6
