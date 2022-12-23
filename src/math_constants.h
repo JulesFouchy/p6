4579d8035b574a38216e97eadd91e3f2538af51a
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-23 13:50:18 +0100

    🧼 [math_constants]

--- src/math_constants.h
+++ src/math_constants.h
@@ -7,8 +7,8 @@ namespace p6 {
  * @{*/
 /* ------------------------------- */
 
-static constexpr float TAU = 6.2831853f;
-static constexpr float PI  = TAU / 2.f;
+[[maybe_unused]] static constexpr float TAU = 6.2831853f;
+[[maybe_unused]] static constexpr float PI  = TAU / 2.f; // NOLINT(readability-identifier-length)
 
 /**@}*/
 } // namespace p6
