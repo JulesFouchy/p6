52ca8afc8687c1e666c07c7e53bd0d16db2b300d
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-23 13:19:51 +0100

    ✨ [random] Implement number()

--- src/random.cpp
+++ src/random.cpp
@@ -1,20 +1,32 @@
 #include "random.h"
+#include <random>
+#include <stdexcept>
 
 namespace p6::random {
 
+static auto& generator()
+{
+    thread_local std::default_random_engine gen{std::random_device{}()};
+    return gen;
+}
+
 float number()
 {
-    return 0.f;
+    return number(0.f, 1.f);
 }
 
 float number(float max)
 {
-    return 0.f;
+    return number(0.f, max);
 }
 
 float number(float min, float max)
 {
-    return 0.f;
+    if (min > max)
+        throw std::invalid_argument{"`min` must be smaller than `max`"};
+
+    const auto distribution = std::uniform_real_distribution<float>{min, max};
+    return distribution(generator());
 }
 
 } // namespace p6::random
--- src/random.h
+++ src/random.h
@@ -6,9 +6,11 @@ namespace p6::random {
 float number();
 
 /// Returns a random number between 0 and `max`.
+/// Throws a `std::invalid_argument` exception if `max < 0`.
 float number(float max);
 
 /// Returns a random number between `min` and `max`.
+/// Throws a `std::invalid_argument` exception if `min > max`.
 float number(float min, float max);
 
 } // namespace p6::random
