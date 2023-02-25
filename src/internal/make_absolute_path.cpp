0c2fd530e1398db8c0c2406cd32fcd40a326ac57
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-25 14:20:16 +0100

    ✨ [path] Throw on invalid path

--- src/internal/make_absolute_path.cpp
+++ src/internal/make_absolute_path.cpp
@@ -1,18 +1,33 @@
 #include "make_absolute_path.h"
 #include <exe_path/exe_path.h>
+#include <filesystem>
+#include <iostream>
+#include <stdexcept>
 
 namespace p6::internal {
 
 std::filesystem::path make_absolute_path(std::filesystem::path path)
 {
-    if (path.is_relative())
+    auto res = [&]() {
+        if (path.is_relative())
+        {
+            return exe_path::dir() / path;
+        }
+        else
+        {
+            return path;
+        }
+    }();
+
+    if (!std::filesystem::exists(res))
     {
-        return exe_path::dir() / path;
+        using namespace std::literals;
+        auto const msg = "Path \""s + res.string() + "\" does not exist. Make sure you didn't misspell it or made it relative to the wrong folder. All relative paths will be relative to your executable's directory.";
+        std::cerr << msg << '\n';
+        throw std::runtime_error{msg};
     }
-    else
-    {
-        return path;
-    }
+
+    return res;
 }
 
 } // namespace p6::internal
