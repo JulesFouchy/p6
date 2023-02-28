f813bb0d1295ad3eb223eebe7275fecc401a9d4b
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-28 01:33:37 +0100

    ✨ [Canvas] Add a depth buffer

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -7,7 +7,7 @@
 namespace p6 {
 
 Canvas::Canvas(ImageSize size, glpp::TextureLayout texture_layout)
-    : _render_target{size, nullptr, texture_layout}
+    : _render_target{size, nullptr, texture_layout, true}
 {}
 
 void save_image(const Canvas& canvas, std::filesystem::path path)
