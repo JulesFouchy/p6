8d9cc3f889c2ada6c80be0f55e1f62ad20d4d270
Jules Fouchy <jules.fouchy@ntymail.com>
2023-04-03 01:55:20 +0200

    ⬆️ [ImGui] Use the official version, main branch, not CoolLab's fork

--- src/internal/ImGuiWrapper.cpp
+++ src/internal/ImGuiWrapper.cpp
@@ -2,9 +2,8 @@
 #include <glad/gl.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_opengl3.h>
-#include <imgui/imgui.h>
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include <imgui/imgui_internal.h>
+#include <imgui/imgui.h>
 
 namespace p6::internal::ImGuiWrapper {
 
